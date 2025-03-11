280c3fe1846a91076440cc20a20190338f8c7747
Zainab <zainabalsaffar03@gmail.com>
2025-03-11 13:16:46 +0300

    Task complete

--- src/cities.ts
+++ src/cities.ts
@@ -53,7 +53,7 @@ const cities: City[] = [
 function getCityPopulation(city: City): number {
   // write your code here...
 
-  return -1; // replace -1 with what you see is fit
+  return city.population; // replace -1 with what you see is fit
 }
 
 /**
@@ -68,7 +68,7 @@ function getCityPopulation(city: City): number {
 function isPopulationAboveThreshold(city: City, threshold: number): boolean {
   // write your code here...
 
-  return false; // replace false with what you see is fit
+  return city.population >= threshold; // replace false with what you see is fit
 }
 
 /**
@@ -97,8 +97,9 @@ function addCity(
   country: string
 ): City[] {
   // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  const newCity: City = { id, name, population, country };
+  cities.push(newCity);
+  return cities; // replace empty array with what you see is fit
 }
 
 /**
@@ -113,7 +114,7 @@ function addCity(
 function countCitiesInCountry(cities: City[], country: string): number {
   // write your code here...
 
-  return -1; // replace -1 with what you see is fit
+  return cities.filter((city) => city.country === country).length; // replace -1 with what you see is fit
 }
 
 export {
